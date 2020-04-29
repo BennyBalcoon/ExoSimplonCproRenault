f86e7ac55e2262d699762f2ba0e428572e04f4b8
Benoit Balcon <benoit.b.balcon@renault.com>
2020-04-29 14:53:44 +0200

    part 1: done - part 2: in progress

--- module-rxjs/02 - Opérateurs de création/exercice-02/exercice-02.js
+++ module-rxjs/02 - Opérateurs de création/exercice-02/exercice-02.js
@@ -1,4 +1,5 @@
 import {Observable} from 'rxjs';
+import {mergeMap} from 'rxjs/operators'
 import {iif, of, range} from 'rxjs';
 
 class Robot {
@@ -15,15 +16,58 @@ class Robot {
 
 let protoRobot = new Robot(Math.floor(Math.random() * 10));
 console.log(protoRobot);
+console.log(protoRobot.id % 2 === 0);
 
-const obs = new Observable(observer => {
-    iif(() => (protoRobot.id % 2 === 0), 
-    observer.next(protoRobot.equipeBleue()),
-    observer.next(protoRobot.equipeRouge())
-    )
+const blue = protoRobot.equipeBleue();
+const red = protoRobot.equipeRouge();
+
+// Avec IF ELSE
+
+// const obs = new Observable(observer => {
+//     if(protoRobot.id % 2 === 0) {
+//         observer.next(protoRobot.equipeBleue());
+//     } else {
+//         observer.next(protoRobot.equipeRouge());
+//     }
+// })
+
+// obs.subscribe((value) => {
+//     console.log(value)
+// });
+
+
+const colorTeam = iif(
+    () => protoRobot.id % 2 === 0,
+    of(blue),
+    of(red)
+)
+
+colorTeam.subscribe((value) => {
+    console.log(value);
 })
 
-obs.subscribe((value) => {
-    console.log(value)
-});
+// OU
+
+const obs = new Observable(observer => observer.next(protoRobot.id));
+
+obs.pipe(mergeMap(value => iif(() => value%2 === 0, of(blue), of(red))))
+    .subscribe(value => console.log(value))
+
+// Partie 2
+
+const source = range(1, 100);
+source.subscribe(() => console.log(new Robot(Math.floor(Math.random() * 10))));
+
+// source.subscribe(() => {
+//     const obs2 = new Observable(observer => observer.next(new Robot(Math.floor(Math.random() * 10))))
+//     obs2.pipe(mergeMap(value => iif(() => value%2 === 0, of(blue), of(red))))
+//     .subscribe(value => console.log(value))
+// });
+
+
+
+
+
+
+
 
